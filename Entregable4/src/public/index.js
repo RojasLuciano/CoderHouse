e1c8ab5572a4e87dc243d7944757015beb188dd8
lucianorojas <Rojaswork@outlook.com>
2023-02-06 18:39:00 -0300

    Terminado entregable 4

---
+++ Entregable4/src/public/index.js
@@ -0,0 +1,47 @@
+const socketClient = io()
+const productForm = document.getElementById('productForm');
+const productDeleteForm = document.getElementById('productDeleteForm')
+
+productForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const formData = new FormData(productForm)
+    fetch('/api', {
+        method: 'POST',
+        body: formData
+    })
+    .then(res => res.json())
+    socketClient.emit('updateProducts')
+})
+
+productDeleteForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const formData = new FormData(productDeleteForm)
+
+    fetch('/api', {
+        method: 'DELETE',
+        body: formData
+    })
+    .then(res => res.json())
+    socketClient.emit('updateProducts')
+})
+
+socketClient.on('fetchProducts', () => {
+    fetch('/api', {
+        method: 'GET'
+    })
+    .then(res => res.json())
+    .then(products => {
+        let realTimeProductList = document.getElementById('realTimeProductList')
+        let productList = products.map(prod => {
+            return `<h3>${prod.title}</h3>
+                    <p>${prod.category}</p>
+                    <p>Descripcion:${prod.description}</p>
+                    <p>Precio: $ ${prod.price}</p>
+                    <p>Codigo: ${prod.code}</p>
+                    <p>Stock: ${prod.stock}</p>
+                    <p>ID: ${prod.id}</p>
+                    <img src=${prod.thumbnail} alt="product image" width="100" height="100"></img>`
+        }).join(' ')
+        realTimeProductList.innerHTML = productList
+    })
+})
